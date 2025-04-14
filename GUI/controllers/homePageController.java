66b68b31a6f6251b15bf78b52685053086d78093
ECiftci1 <emreciftci2001@hotmail.com>
2025-04-14 23:41:29 +0200

    added needed views

--- GUI/controllers/homePageController.java
+++ GUI/controllers/homePageController.java
@@ -11,6 +11,11 @@ public class homePageController {
     }
 
     @FXML
+    private void goToHomePage(ActionEvent event) {
+        mainController.switchScene("HomePage.fxml");
+    }
+
+    @FXML
     private void goToModuleProgressAll(ActionEvent event) {
         mainController.switchScene("moduleProgressAll.fxml");
     }
@@ -21,6 +26,26 @@ public class homePageController {
     }
 
     @FXML
+    private void goToAllModules(ActionEvent event) {
+        mainController.switchScene("allModules.fxml");
+    }
+    
+    @FXML
+    private void goToWebcastProgressAll(ActionEvent event) {
+        mainController.switchScene("webcastProgressAll.fxml");
+    }
+
+    @FXML
+    private void goToWebcastProgressSelected(ActionEvent event) {
+        mainController.switchScene("webcastProgressSelected.fxml");
+    }
+
+    @FXML
+    private void goToAllWebcasts(ActionEvent event) {
+        mainController.switchScene("allWebcasts.fxml");
+    }
+
+    @FXML
     private void goToTopWebcasts(ActionEvent event) {
         mainController.switchScene("topWebcasts.fxml");
     }
@@ -28,5 +53,25 @@ public class homePageController {
     @FXML
     private void goToCompletedCourse(ActionEvent event) {
         mainController.switchScene("completedCourse.fxml");
+    }
+
+    @FXML
+    private void goToStudents(ActionEvent event) {
+        mainController.switchScene("manageStudents.fxml");
+    }
+
+    @FXML
+    private void goToEnrollments(ActionEvent event) {
+        mainController.switchScene("manageEnrollments.fxml");
+    }
+
+    @FXML
+    private void goToAddEnrollment(ActionEvent event) {
+        mainController.switchScene("addEnrollment.fxml");
+    }
+
+    @FXML
+    private void goToAddStudent(ActionEvent event) {
+        mainController.switchScene("addStudent.fxml");
     }
 }
